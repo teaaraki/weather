607fe19893ae918afc76b481e3af9829931cb263
Tea Araki <teaaraki@hawaii.edu>
2025-03-12 03:53:32 +0000

    quesiton 3

--- index.js
+++ index.js
@@ -6,47 +6,63 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));
 
-//displays index.html of root path
-app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/index.html")
+const apiKey = process.env.API_KEY || "95d99161b648050e7c09e73645f7314d";
+const units = "imperial";
+
+app.get("/", function (req, res) {
+    res.sendFile(__dirname + "/index.html");
 });
 
 //invoked after hitting go in the html form
-app.post("/", function(req, res) {
-    
-    // takes in the zip from the html form, display in // console. Takes in as string, ex. for zip 02139
-        var zip = String(req.body.zipInput);
-        console.log(req.body.zipInput);
-    
-    //build up the URL for the JSON query, API Key is // secret and needs to be obtained by signup 
-        const units = "imperial";
-        const apiKey = "67f6b382921c1e89b39b20d4f9556f22";
-        const url = "https://api.openweathermap.org/data/2.5/weather?zip=" + zip +  "&units=" + units + "&APPID=" + apiKey;
-    
-    // this gets the data from Open WeatherPI
-    https.get(url, function(response){
-        console.log(response.statusCode);
-        
-        // gets individual items from Open Weather API
-        response.on("data", function(data){
+app.post("/", function (req, res) {
+    const zip = req.body.zipInput.trim();
+    const cityId = req.body.cityIdInput.trim();
+    let url = "";
+
+    //gets infor from api weather 
+    if (cityId) {
+        url = `https://api.openweathermap.org/data/2.5/weather?id=${cityId}&units=${units}&APPID=${apiKey}`;
+    } else if (zip) {
+        url = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&units=${units}&APPID=${apiKey}`;
+    } else {
+        return res.send("<h3>Please enter a Zip Code or City ID.</h3>");
+    }
+
+    https.get(url, function (response) {
+        console.log("Response Status Code:", response.statusCode);
+
+        if (response.statusCode !== 200) {
+            return res.send("<h3>Invalid Zip Code or City ID. Try Again.</h3>");
+        }
+
+        //shows differet weather info
+        response.on("data", function (data) {
             const weatherData = JSON.parse(data);
-            const temp = weatherData.main.temp;
+
             const city = weatherData.name;
+            const temp = weatherData.main.temp;
+            const humidity = weatherData.main.humidity;
+            const windSpeed = weatherData.wind.speed;
+            const windDirection = weatherData.wind.deg;
+            const cloudiness = weatherData.clouds.all;
             const weatherDescription = weatherData.weather[0].description;
             const icon = weatherData.weather[0].icon;
-            const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
-            
-            // displays the output of the results
-            res.write("<h1> The weather is " + weatherDescription + "<h1>");
-            res.write("<h2>The Temperature in " + city + " " + zip + " is " + temp + " Degrees Fahrenheit<h2>");
-            res.write("<img src=" + imageURL +">");
+            const imageURL = `http://openweathermap.org/img/wn/${icon}@2x.png`;
+
+            displays different weather info
+            res.write(`<h1>Weather in ${city}</h1>`);
+            res.write(`<h2>${weatherDescription}</h2>`);
+            res.write(`<h2>Temperature: ${temp} °F</h2>`);
+            res.write(`<h2>Humidity: ${humidity}%</h2>`);
+            res.write(`<h2>Wind Speed: ${windSpeed} mph</h2>`);
+            res.write(`<h2>Wind Direction: ${windDirection}°</h2>`);
+            res.write(`<h2>Cloudiness: ${cloudiness}%</h2>`);
+            res.write(`<img src="${imageURL}">`);
             res.send();
         });
     });
-})
+});
 
-
-//Code will run on 3000 or any available open port
-app.listen(process.env.PORT || 3000, function() {
-console.log ("Server is running on port")
+app.listen(3000, function () {
+    console.log("Server is running on port 3000");
 });
